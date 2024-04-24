128f51d8b781f7201a0d42e3e605be295d1eb715
Ahmeddhibi19 <adhibi345@gmail.com>
2024-04-24 19:11:10 +0200

    latest

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -90,12 +90,12 @@ node(){
         }
         /* Maven - tests */
         stage('SERVICE - Tests unitaires'){
-            sh 'docker run --rm --name maven-${commitIdLong} -v /var/lib/jenkins/maven/:/root/.m2 -v "$(pwd)":/usr/src/mymaven --network generator_generator -w /usr/src/mymaven maven:3.8.3-jdk-17 mvn -B clean test'
+            sh 'docker run --rm --name maven-${commitIdLong} -v /var/lib/jenkins/maven/:/root/.m2 -v "$(pwd)":/usr/src/mymaven --network generator_generator -w /usr/src/mymaven maven:3.8.3-openjdk-17 mvn -B clean test'
         }
 
         /* Maven - build */
         stage('SERVICE - Jar'){
-            sh 'docker run --rm --name maven-${commitIdLong} -v /var/lib/jenkins/maven/:/root/.m2 -v "$(pwd)":/usr/src/mymaven --network generator_generator -w /usr/src/mymaven maven:3.8.3-jdk-17 mvn -B clean install'
+            sh 'docker run --rm --name maven-${commitIdLong} -v /var/lib/jenkins/maven/:/root/.m2 -v "$(pwd)":/usr/src/mymaven --network generator_generator -w /usr/src/mymaven maven:3.8.3-openjdk-17 mvn -B clean install'
         }
 
 

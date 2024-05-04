c3c1bddbeab3854924f93c984febb66fda3edee9
Ahmeddhibi19 <adhibi345@gmail.com>
2024-05-04 22:24:56 +0200

    latest

--- src/main/java/com/PFA2/EduHousing/Utils/Constants.java
+++ src/main/java/com/PFA2/EduHousing/Utils/Constants.java
@@ -2,5 +2,5 @@ package com.PFA2.EduHousing.Utils;
 
 public class Constants {
     public static final String APP_ROOT="/EduHousing/v1.0.0";
-    public static final String AUTHENTICATION_ENDPOINT=APP_ROOT+"/auth";
+    public static final String AUTHENTICATION_ENDPOINT= APP_ROOT+"/auth";
 }

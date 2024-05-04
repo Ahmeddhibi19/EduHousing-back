6f9441f8a45ffdf9a2bad9d9bf53a2dd6f6fb69d
Ahmeddhibi19 <adhibi345@gmail.com>
2024-05-04 03:05:30 +0200

    latest

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -96,7 +96,7 @@ node(){
         /* Maven - build */
         stage('SERVICE - Jar'){
             sh 'docker run --rm --name maven-${commitIdLong} -v /var/lib/jenkins/maven/:/root/.m2 -v "$(pwd)":/usr/src/mymaven --network generator_generator -w /usr/src/mymaven maven:3.8.3-openjdk-17 mvn -B clean install'
-            sh 'sudo mv target/*.jar target/app.jar'
+            sh ' mv target/*.jar target/app.jar'
         }
 
         /* Docker - build & push */
